6f2111c86c753c44b8b7379a6ff0564e2c5480d3
Lucas Da Pieve <lucasdapievesilva@gmail.com>
2020-12-03 08:35:03 -0300

    agenda_contatos

---
+++ agenda_contatos/lib/helpers/contact_helper.dart
@@ -0,0 +1,47 @@
+import 'package:sqflite/sqflite.dart';
+
+final String idColumn = "idColumn";
+final String nameColumn = "nameColumn";
+final String emailColumn = "emailColumn";
+final String phoneColumn = "phoneColumn";
+final String imgColumn = "imgColumn";
+
+class ContactHelper {}
+
+// id  name  email  phone  img
+// 0  Lucas  lucas@  47   URL
+
+class Contact {
+  int id;
+//Dados que serão inseridos no mapa
+  String name;
+  String email;
+  String phone;
+  String img;
+//Contrutor que vincula os dados que são digitaos em cada index do mapa
+  Contact.fromMap(Map map) {
+    id = map[idColumn];
+    name = map[nameColumn];
+    email = map[emailColumn];
+    phone = map[phoneColumn];
+    img = map[imgColumn];
+  }
+// Função que retorna os dados armazenados no mapa
+  Map toMap() {
+    Map<String, dynamic> map = {
+      nameColumn: name,
+      emailColumn: email,
+      phoneColumn: phone,
+      imgColumn: img
+    };
+    if (id != null) {
+      map[idColumn] = id;
+    }
+    return map;
+  }
+
+  @override
+  String toString() {
+    return "Contact(id: $id, name: $name, email: $email, phone: $phone, img: $img)";
+  }
+}
