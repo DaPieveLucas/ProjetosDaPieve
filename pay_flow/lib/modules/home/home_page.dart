4364e5b743db24e0fc7aefc326e40f654331091e
Lucas Da Pieve <lucasdapievesilva@hotmail.com>
2021-07-08 19:38:48 -0300

    aula 2

--- pay_flow/lib/modules/home/home_page.dart
+++ pay_flow/lib/modules/home/home_page.dart
@@ -1,16 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:pay_flow/modules/home/home_controller.dart';
+import 'package:pay_flow/shared/themes/app_colors.dart';
+import 'package:pay_flow/shared/themes/app_text_styles.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('HOME PAGE'),
+    final controller = HomeController();
+    final pages = [
+      Container(
+        color: Colors.red,
       ),
-      body: Container(
-        color: Colors.amber,
+      Container(
+        color: Colors.blue,
+      )
+    ];
+
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(152),
+        child: Container(
+          height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(TextSpan(
+                  text: 'Olá, ',
+                  style: TextStyles.titleRegular,
+                  children: [
+                    TextSpan(
+                        text: 'Lucas', style: TextStyles.titleBoldBackground)
+                  ])),
+              subtitle: Text(
+                'Mantenha as suas contas em dia',
+                style: TextStyles.captionShape,
+              ),
+              trailing: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: pages[controller.currentPage],
+      bottomNavigationBar: Container(
+        height: 90,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(
+                onPressed: () {
+                  controller.setPage(0);
+                  setState(() {});
+                },
+                icon: Icon(
+                  Icons.home,
+                  color: AppColors.primary,
+                )),
+            GestureDetector(
+              onTap: () {
+                print('CLICOU');
+              },
+              child: Container(
+                height: 56,
+                width: 56,
+                decoration: BoxDecoration(
+                    color: AppColors.primary,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Icon(
+                  Icons.add_box_outlined,
+                  color: AppColors.background,
+                ),
+              ),
+            ),
+            IconButton(
+                onPressed: () {
+                  setState(() {
+                    controller.setPage(1);
+                  });
+                },
+                icon: Icon(
+                  Icons.description_outlined,
+                  color: AppColors.body,
+                )),
+          ],
+        ),
       ),
     );
   }
--- pay_flow/lib/modules/login/login_page.dart
+++ pay_flow/lib/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pay_flow/modules/login/login_controller.dart';
 import 'package:pay_flow/shared/themes/app_colors.dart';
 import 'package:pay_flow/shared/themes/app_images.dart';
 import 'package:pay_flow/shared/themes/app_text_styles.dart';
@@ -12,6 +13,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -60,8 +63,7 @@ class _LoginPageState extends State<LoginPage> {
                     padding:
                         const EdgeInsets.only(left: 20, right: 20, top: 20),
                     child: SocialLoginButton(
-                      onTap: () {},
-                    ),
+                        onTap: () => controller.googleSingIn(context)),
                   ),
                 ],
               ),
